4fae29764ad2d1443599ccc2e66a949072610e79
Adila Abudureheman <aabudureheman@gmail.com>
2022-11-29 14:03:27 -0500

    cookie working

--- background.js
+++ background.js
@@ -3,26 +3,26 @@ var cookie;
 var body = [];
 // Not sure this listener is right
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    
     if (changeInfo.status == 'complete') {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             var tab = tabs[0];
             var url = new URL(tab.url)
             domain = url.hostname
             console.log(domain);
-            chrome.cookies.get({"url":domain, "name":"RememberMe"}, function(cookies) {
+            chrome.cookies.getAll({"domain":domain}, function(cookies) {
                         console.log("The cookie is");
                         console.log(cookies);
-                        
                         cookie = cookies;
                  });
           });
-
     }
   })
 
   chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       var fileName = details.url;
+      console.log(JSON.stringify(details));
       if (details.requestBody) {
         if (details.requestBody.raw) {
             console.log(fileName);
